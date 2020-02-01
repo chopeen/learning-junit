2fc90eaf0fc1a4308051b47280c428cf8da283f7
chopeen <chopeen@gmail.com>
2020-02-01 23:51:12 +0100

    added a parameterized unit test;

---
+++ junit5/src/test/java/org/chopeen/learning/CalculatorTest.java
@@ -0,0 +1,22 @@
+package org.chopeen.learning;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CalculatorTest {
+    private static Calculator calc;
+
+    @BeforeAll
+    public static void setup() {
+        calc = new Calculator();
+    }
+
+    @ParameterizedTest(name = "Result for `{0}` should be positive")
+    @ValueSource(ints = { 0, 1, 2, 3 })
+    public void test_getSquare(int num) {
+        assertTrue(calc.getSquare(num) > 0);
+    }
+}
