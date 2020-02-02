da997a4491a9579cba93fc100776a199a57073d2
chopeen <chopeen@gmail.com>
2020-02-02 16:06:48 +0100

    renamed the test file;

---
+++ junit5/src/test/java/org/chopeen/learning/CalculatorTest5.java
@@ -0,0 +1,24 @@
+package org.chopeen.learning;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CalculatorTest5 {
+    private static Calculator calc;
+
+    @BeforeAll
+    public static void setup() {
+        calc = new Calculator();
+    }
+
+    @DisplayName("Square")
+    @ParameterizedTest(name = "Result for `{0}` should be positive")
+    @ValueSource(ints = { 1, 2, 3 })
+    public void test_getSquare(int num) {
+        assertTrue(calc.getSquare(num) > 0);
+    }
+}
