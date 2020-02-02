6b654770eab1179ace2be08b7839445eff2d09a1
chopeen <chopeen@gmail.com>
2020-02-02 17:06:18 +0100

    tests must begin or end with `Test` for Maven to find them;
    - https://stackoverflow.com/a/6178629/95

--- junit4/src/test/java/org/chopeen/learning/CalculatorTest.java
+++ junit4/src/test/java/org/chopeen/learning/CalculatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class CalculatorTest4 {
+public class CalculatorTest {
     private static Calculator calc;
 
     @BeforeClass
--- junit5/src/test/java/org/chopeen/learning/CalculatorTest.java
+++ junit5/src/test/java/org/chopeen/learning/CalculatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class CalculatorTest5 {
+public class CalculatorTest {
     private static Calculator calc;
 
     @BeforeAll
