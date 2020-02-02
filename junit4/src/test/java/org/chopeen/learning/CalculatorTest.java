7340b070e378b32e54ec91055b4fc8dc409489a7
chopeen <chopeen@gmail.com>
2020-02-02 17:47:06 +0100

    paramaterized the JUnit 4 test;

--- junit4/src/test/java/org/chopeen/learning/CalculatorTest.java
+++ junit4/src/test/java/org/chopeen/learning/CalculatorTest.java
@@ -2,21 +2,36 @@ package org.chopeen.learning;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class CalculatorTest {
     private static Calculator calc;
+    private Integer parameterValue;
+
+    public CalculatorTest(Integer value) {
+        this.parameterValue = value;
+    }
 
     @BeforeClass
     public static void setup() {
         calc = new Calculator();
     }
 
+    @Parameters
+    public static Collection<Integer> parameterData() {
+        return Arrays.asList(1, 2, 3);
+    }
+
     @Test
     public void test_getSquare() {
-        assertTrue(calc.getSquare(1) > 0);
-        assertTrue(calc.getSquare(2) > 0);
-        assertTrue(calc.getSquare(3) > 0);
+        assertTrue(calc.getSquare(this.parameterValue) > 0);
     }
 }
